52cf2b075d668ee5aa14c2635e88e61a9fa2c2c3
danieletapparelli <51820976+danieletapparelli@users.noreply.github.com>
2020-09-09 12:46:03 +0200

    Add files via upload

---
+++ simulator.py
@@ -0,0 +1,22 @@
+from map import Map
+
+class Simulator:
+    def __init__(self, topology, nodes):
+        self.topology = topology
+        self.time = 0
+        self.nodes = nodes
+        
+    def start_simulator(self):
+        
+        
+        nodes = []
+        
+        
+            
+            
+
+            
+        
+    
+
+
